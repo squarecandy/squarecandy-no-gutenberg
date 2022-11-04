aaa5df1a2807457f96ba17a7e8484f62c81abc91
Peter Wise <peter@squarecandy.net>
2022-11-04 14:42:28 -0400

    feat: remove gutenberg front end styles and SVG markup

--- squarecandy-no-gutenberg.php
+++ squarecandy-no-gutenberg.php
@@ -11,4 +11,20 @@ Author: squarecandy
 Copyright 2022 Peter T. Wise D.B.A. Square Candy Design
 See LICENSE file
 */
-add_filter('use_block_editor_for_post', '__return_false', 10);
+add_filter( 'use_block_editor_for_post', '__return_false', 10 );
+
+// REMOVE GUTENBERG BLOCK LIBRARY CSS FROM LOADING ON FRONTEND
+// see https://github.com/WordPress/gutenberg/issues/24684#issuecomment-1022370212
+function squarecandy_remove_wp_block_library_css() {
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'wp-block-library-theme' );
+	wp_dequeue_style( 'wc-block-style' ); // REMOVE WOOCOMMERCE BLOCK CSS
+	wp_dequeue_style( 'global-styles' ); // REMOVE THEME.JSON
+	wp_dequeue_style( 'classic-theme-styles' );
+}
+add_action( 'wp_enqueue_scripts', 'squarecandy_remove_wp_block_library_css', 100 );
+
+// remove duotone SVG markup
+// see https://github.com/WordPress/gutenberg/issues/38299#issuecomment-1223860368
+remove_filter( 'render_block', 'wp_render_duotone_support', 999 );
+remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
